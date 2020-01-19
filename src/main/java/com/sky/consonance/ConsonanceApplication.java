9dadd1489bc8a2e14c82cc23e62207e5a990d65d
Kiselev SE <semyon.k@sunrise-r.ru>
2020-01-19 16:50:52 +0300

    Initial commit

---
+++ src/main/java/com/sky/consonance/ConsonanceApplication.java
@@ -0,0 +1,13 @@
+package com.sky.consonance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsonanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsonanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sky/consonance/ConsonanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.consonance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsonanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
